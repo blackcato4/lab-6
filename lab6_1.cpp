8b373c7892e0a3cea4c029b9e563bd56c08fa0a1
Phakbung <athikan_thoyaem@cmu.ac.th>
2025-12-09 09:03:25 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,28 @@
 #include<iostream>
 using namespace std;
 
+
 int main(){
+    int integer = 1 ; 
+    int evenCount = 0;
+    int oddCount = 0;
+   
+    while (true){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> integer ;
+    if ( integer == 0) {
+        break ;
+    }
+
+
+        if (integer % 2 == 0) {
+            evenCount++;
+        } else {    
+            oddCount++;
+        }
+    
+    }
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
     return 0;
 }
